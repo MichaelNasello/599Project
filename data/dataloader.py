8313c38b515fe6e1ca156c33dbb17026f401b7ce
MichaelNasello <mlnasell@uwaterloo.ca>
2022-11-22 16:52:40 -0500

    updates

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,3 +1,5 @@
+import os.path
+
 import pandas as pd
 
 
@@ -60,7 +62,11 @@ def load_patient_task_data_from_txt(patient_id, task_num):
         patient_folder = f'{patient_id.split("-")[0]}/OFF_{patient_id.split("-")[1]}'
     else:
         patient_folder = patient_id
-    data = pd.read_table(f'raw_data/{patient_folder}/task_{task_num}.txt', sep=',')
+
+    if os.path.exists(f'raw_data/{patient_folder}/task_{task_num}.txt'):
+        data = pd.read_table(f'raw_data/{patient_folder}/task_{task_num}.txt', sep=',')
+    else:
+        return pd.DataFrame()
 
     data = data.drop('0', axis=1)
     data = data.set_axis(headers, axis=1)
@@ -69,6 +75,9 @@ def load_patient_task_data_from_txt(patient_id, task_num):
 
 
 def clean_and_verify(raw_data):
+    if raw_data.empty:
+        return raw_data
+
     eeg_columns = [
         'FP1', 'FP2', 'F3', 'F4', 'C3', 'C4', 'P3', 'P4', 'O1', 'O2', 'F7', 'F8', 'P7', 'P8', 'FZ', 'CZ', 'PZ', 'FC1',
         'FC2', 'CP1', 'CP2', 'FC5', 'FC6', 'CP5', 'CP6'
@@ -77,6 +86,17 @@ def clean_and_verify(raw_data):
     for i in range(3):
         raw_data = raw_data.drop(f'NC_invalid_{i}', axis=1)
 
+    # Right and left shank not both included
+    for axis in ['x', 'y', 'z']:
+        for sensor in ['acc', 'gyro']:
+            col = f'{sensor}_{axis}_left_shank'
+            if raw_data[col].values.mean() != 0 and raw_data[col].values.std() != 0:
+                raw_data[f'{sensor}_{axis}_shank'] = raw_data[col].values
+                raw_data = raw_data.drop([col, f'{sensor}_{axis}_right_shank'], axis=1)
+            else:
+                raw_data[f'{sensor}_{axis}_shank'] = raw_data[f'{sensor}_{axis}_right_shank'].values
+                raw_data = raw_data.drop([col, f'{sensor}_{axis}_right_shank'], axis=1)
+
     for col in raw_data.columns[1:]:
         # Not using EEG data
         if col in eeg_columns:
@@ -84,7 +104,7 @@ def clean_and_verify(raw_data):
             continue
 
         if raw_data[col].values.mean() == 0 and raw_data[col].values.std() == 0:
-            print(f'Dropping {col} (has no useful information)')
+            # print(f'Dropping {col} (has no useful information)')
 
             raw_data = raw_data.drop(col, axis=1)
 
